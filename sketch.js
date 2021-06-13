f1b92d0c0df142fd7a8bfd0bbc34ffebfbcc8e53
hafsajan <79843377+hafsajan@users.noreply.github.com>
2021-06-13 19:08:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function preload()
 	bgImg = loadImage("starNight.png");
 
 	//load animation for fairy here
-	fairyimage=loadAnimation("fairyimage1.png","fairyimage2.png");
+	fairyimage=loadAnimation("fairyImage1.png","fairyImage2.png");
 	
 	//load sound here
 	music=loadSound("JoyMusic.mp3");
